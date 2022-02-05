74abf54f8bb5818546d4767dc0317822d1b7abbb
GrahamNeal13 <91097552+GrahamNeal13@users.noreply.github.com>
2022-02-05 14:27:22 -0700

    deliverable 2 complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -42,5 +42,21 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY count DESC;
 
-
+--Create mentorship_eligibilty table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON de.emp_no = e.emp_no
+INNER JOIN titles as ti
+on e.emp_no = ti.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
 
